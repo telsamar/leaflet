8a2e7d8a71d0d881f4324cfd96cce1096c7e1f65
telsamar <56135473+telsamar@users.noreply.github.com>
2024-06-12 17:34:41 +0300

    Update server_socket.js

--- server/services/server_socket.js
+++ server/services/server_socket.js
@@ -1,40 +1,51 @@
+const markers = [
+    [55.751244, 37.618423],
+    [55.760186, 37.618698]
+];
+
 exports.server_socket = {
     io: null,
     sockets: [],
 
     create(socketIo) {
-        console.log('+ Creating socket server')
+        console.log('+ Creating socket server');
         this.io = socketIo;
         this.io.on('connection', (socket) => {
-            console.log('client connected: ', socket.id)
-            this.sockets.push(socket)
+            console.log('client connected: ', socket.id);
+            this.sockets.push(socket);
 
-            socket.on('disconnect',(reason)=>{
-               this.sockets = [...this.sockets.filter( s => s.id !== socket.id)]
-                console.log(`Client ${socket.id} disconected, because: ${reason}`)
-            })
-        })
-        return this.io
+            console.log('Sending initial markers to client: ', markers);
+            socket.emit('initMarkers', markers); // Теккущие маркеры
 
+            socket.on('disconnect', (reason) => {
+                this.sockets = this.sockets.filter(s => s.id !== socket.id);
+                console.log(`Client ${socket.id} disconnected, because: ${reason}`);
+            });
+
+            socket.on('addMarker', (marker) => {
+                markers.push(marker);
+                console.log('New marker added: ', marker);
+                this.io.emit('newMarker', marker);  // Оповещаем всех пользователей о новом маркере
+            });
+        });
+        return this.io;
     },
 
-
     sendSignalToAllClients() {
-        console.log('send message')
-        this.sockets.map( s => {
+        console.log('send message');
+        this.sockets.map(s => {
             s.emit('signal', '!')    // Set name socket
         })
     },    
     
-
     sendInfoToOneClient(info, socket_id) {
-        console.log('send info')
-        console.log(socket_id)
+        console.log('send info');
+        console.log(socket_id);
         
-        this.sockets.map( s => {
-            for ( i of s.client.sockets.keys()) {
+        this.sockets.map(s => {
+            for (let i of s.client.sockets.keys()) {
                 if (i === socket_id) {
-                    console.log('send info to client with id = '+i+' ('+socket_id+')')
+                    console.log('send info to client with id = ' + i + ' (' + socket_id + ')');
                     s.emit('message', info)    // Set name socket
                 }
             }
